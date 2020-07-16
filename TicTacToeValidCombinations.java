b78d3afcbb1b3b5522b288d2c6c7ba23bf327cd2
novakpajovic <51486199+novakpajovic@users.noreply.github.com>
2020-07-16 13:30:16 +0200

    Update TicTacToeValidCombinations.java

--- TicTacToeValidCombinations.java
+++ TicTacToeValidCombinations.java
@@ -54,21 +54,18 @@ public class TicTacToeValidCombinations {
 						validCombinations++;
 						myBoards.add(strBuilder1.toString());
 						
-						//out.println(strBuilder1.toString());
+					;
 						
 					}
 					
 					
 					
 					
-				//System.out.println(strBuilder1.toString());
-				//	out.println(strBuilder1.toString());
-					
 					
 				}
 			}
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
     }
@@ -85,26 +82,25 @@ public class TicTacToeValidCombinations {
     		}
     		
     	}catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
     	}
     }
     
-    static int win[][] = {{0, 1, 2, 3}, // Check first row.  
-    	    {4, 5, 6, 7}, // Check second Row  
-    	    {8, 9, 10, 11}, // Check third Row  
-    	    {12, 13, 14, 15}, // Check fourth Row
-    	    {0, 4, 8, 12},// Check first Column
-    	    {1, 5, 9, 13}, // Check second Column  
-    	    {2, 6, 10, 14}, // Check third Column
-    	    {3, 7, 11, 15}, // Check fourth Column
-    	    {0, 5, 10, 15}, // Check first Diagonal  
-    	    {3, 6, 9, 12}}; // Check second Diagonal  
+    static int win[][] = {{0, 1, 2, 3},   
+    	    {4, 5, 6, 7},   
+    	    {8, 9, 10, 11},   
+    	    {12, 13, 14, 15},
+    	    {0, 4, 8, 12},
+    	    {1, 5, 9, 13},  
+    	    {2, 6, 10, 14}, 
+    	    {3, 7, 11, 15}, 
+    	    {0, 5, 10, 15}, 
+    	    {3, 6, 9, 12}}; 
     	  
-    	// Returns true if character 'c' wins. c can be either  
-    	// 'X' or 'O'  
+   
     	    static boolean isCWin(char[] board, char c) { 
-    	        // Check all possible winning combinations  
+    	          
     	        for (int i = 0; i < 10; i++) { 
     	            if (board[win[i][0]] == c 
     	                    && board[win[i][1]] == c 
@@ -117,7 +113,7 @@ public class TicTacToeValidCombinations {
     	    } 
     
     static boolean isValid(char board[]) { 
-        // Count number of 'X' and 'O' in the given board  
+         
         int xCount = 0, oCount = 0, plusCount = 0; 
         for (int i = 0; i < 16; i++) { 
             if (board[i] == 'X') { 
@@ -132,14 +128,12 @@ public class TicTacToeValidCombinations {
             } 
         } 
   
-        // Board can be valid only if either xCount and oCount  
-        // is same or xount is one more than oCount  
+  
         if (xCount == oCount || xCount == plusCount || xCount == oCount + 1 || xCount == plusCount + 1 || oCount == plusCount + 1) 
         { 
-            // Check if 'O' is winner  
+             
             if (isCWin(board, 'O')) { 
-                // Check if 'X' is also winner, then  
-                // return false  
+                 
                 if (isCWin(board, 'X')) { 
                     return false; 
                 }
@@ -148,34 +142,30 @@ public class TicTacToeValidCombinations {
                     return false; 
                 }
   
-                // Else return true xCount and yCount are same  
+                 
                 return (xCount == oCount && oCount == plusCount + 1); 
             }
             
-         // Check if '+' is winner  
+           
             if (isCWin(board, '+')) { 
-                // Check if 'X' is also winner, then  
-                // return false  
+               
                 if (isCWin(board, 'X')) { 
                     return false; 
                 }
-                
-             // Check if 'O' is also winner, then  
-                // return false
+                                   
                 if (isCWin(board, 'O')) { 
                     return false; 
                 }
   
-                // Else return true oCount and plusCount are same  
+                  
                 return (xCount == plusCount && oCount == plusCount);
             } 
-  
-            // If 'X' wins, then count of X must be greater  
+              
             if (isCWin(board, 'X') && (xCount != oCount + 1 || xCount != plusCount + 1)) { 
                 return false; 
             }
   
-            // If 'O' is not winner, then return true  
+              
             return true; 
         } 
         return false; 
